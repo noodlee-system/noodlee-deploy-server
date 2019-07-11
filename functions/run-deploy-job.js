a26ddd511238c198779e2e9519bab80d165fd73a
SkrzypMajster <skrzypmajster@gmail.com>
2019-07-11 07:11:36 +0200

    Install and try out shelljs package

--- functions/run-deploy-job.js
+++ functions/run-deploy-job.js
@@ -1,25 +1,21 @@
-const { exec } = require('child_process');
+const shell = require('shelljs');
 
 function runDeployJob(jobObject) {
     console.log(`${jobObject.name} job with id ${jobObject.id} started...`);
 
-    exec(`git clone ${jobObject.repoUrl} -b ${jobObject.deploymentBranch} ../${jobObject.directoryName}`, (err) => {
-        if (err) {
-            // node couldn't execute the command
-            console.error(`${jobObject.name} job with id ${jobObject.id} was executed with error...`);
-            return;
-        }
-        
-        exec(`cd ../${jobObject.directoryName} && docker-compose up --build`, (err) => {
-            if (err) {
-                // node couldn't execute the command
-                console.error(`${jobObject.name} job with id ${jobObject.id} was executed with error...`);
-                return;
-            }
+    if (!shell.which('git') || !shell.which('docker')) {
+        console.error('ERROR: Sorry, deploy scripts requires git and docker!');
+        return;
+    }
 
-            console.log(`${jobObject.name} job with id ${jobObject.id} was executed successfully...`);
-        });
-    });
+    shell.cd("..");
+
+    if (shell.exec(`git clone ${jobObject.repoUrl} -b ${jobObject.deploymentBranch}`).code !== 0) {
+        console.error('ERROR: Cloning repository failed!');
+        return;
+    }
+
+    console.log(`${jobObject.name} job with id ${jobObject.id} ended successfully...`);
 }
 
 module.exports = runDeployJob;
